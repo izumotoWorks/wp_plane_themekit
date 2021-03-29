9c352fc1c80497f5683ded3691e5f5bd4397ac36
izumotoWorks <izumoto.works@gmail.com>
2021-03-29 11:45:58 +0900

    fix

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 
 <? wp_head(); ?>
 
-    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css?<?php echo file_date(get_template_directory() . '/style.css'); ?>" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css" type="text/css">
     <title><?php if(is_home()){ bloginfo('name'); } ?><?php if(is_single()){ the_title(); } ?><?php if(is_category()){ single_cat_title();} ?><?php if(is_category()){ echo ' | ';} ?><?php if(is_category()){ bloginfo('name');} ?><?php if(is_tag()){ single_tag_title();} ?><?php if(is_tag()){ echo ' | ';} ?><?php if(is_tag()){ bloginfo('name');} ?><?php if(is_page()){ the_title();} ?><?php if(is_page()){ echo ' | ';} ?><?php if(is_page()){ bloginfo('name');} ?></title>
   </head>
   <body>
